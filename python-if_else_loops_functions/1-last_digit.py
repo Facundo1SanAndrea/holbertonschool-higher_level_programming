6925e07b2b2ef0461de86b932c580619529ccf43
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-07 19:51:01 -0300

    fixing negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-if (number > 0):
-    last_digit = -last_digit
+if number >= 0:
+    last_digit = int(repr(number)[-1])
+else:
+    last_digit = int(repr(number)[-1]) * -1
 if last_digit > 5 and last_digit != 0:
     print(f"Last digit of {number} is {number % 10} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is 0 and is 0")
-elif last_digit < 5 and last_digit != 0:
+elif last_digit < 6 and last_digit != 0:
     print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
