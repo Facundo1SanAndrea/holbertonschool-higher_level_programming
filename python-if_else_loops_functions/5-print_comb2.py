36418e3a0b047d9f72e557af3e3e0e23caa80f19
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-08 14:35:16 -0300

    Adding task 6 of list

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/bin/python3
-for number in range (00, 99):
-  print("{}".format(number), end=' ')
+for number in range (0, 10):
+  for x in range (0, 10):
+    print("{}{}".format(number, x), end=' ')
