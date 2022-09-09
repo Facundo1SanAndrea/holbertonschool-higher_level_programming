47e8abc50a5745f4cc0349658c9367a46f95c191
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-09 11:20:46 -0300

    fixing line and task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for number in range (0, 10):
-  for x in range (0, 10):
-    print("{}{}".format(number, x), end=' ')
+for number in range (0,99):
+  print("{:02}, ".format(number), end="")
+print("99")
