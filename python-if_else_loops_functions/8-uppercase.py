d6c3886b62736cd276c2f676ad41e22a5cb8c283
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-09 07:35:37 -0700

    Adding pepo8 and fixing

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+islower = __import__('7-islower').islower
+
+
+def uppercase(str):
+    closestring = ''
+    for a in str:
+        if islower(a) is True:
+            closestring += (chr(ord(a) - 32))
+        else:
+            closestring += a
+    print("{}".format(closestring))
