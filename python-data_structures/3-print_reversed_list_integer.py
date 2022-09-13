85cddfea8fe5966795ff87f9460e765676d2f032
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 10:50:29 -0300

    Fixing returing point in case of none

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 
+from ast import Return
+
+
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     my_list.reverse()
 
     for i in my_list:
