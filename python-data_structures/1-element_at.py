9f9b9e347aaf6755e4db4fe1ebe5c4fade17f425
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 09:46:26 -0300

    fixing return

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
-        return
+        return None
     for i in my_list:
         if i == idx:
             return(my_list[i])
