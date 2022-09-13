448ee2dadd234d7f6e66bcab553b0ee3f8b8bb3c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 11:30:10 -0300

    addding function tat removes

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def no_c(my_string):
+    newstring =""
+
+    for i in my_string:
+        if (i != "c") and (i != "C"):
+            newstring += 1
+
+    return newstring
