5aa3a7202f81238b8c3bb18ad7c162311bed31c1
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 10:11:44 -0300

    Adding function to the git

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from multiprocessing.sharedctypes import Value
+from unittest.loader import VALID_MODULE_NAME
+
+
+def replace_in_list(my_list, idx, element):
+    if ((idx < 0) or (idx >= len(my_list))):
+        return my_list
+        
+    my_list[idx] = element
+    return my_list
