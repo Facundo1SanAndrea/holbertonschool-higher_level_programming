0c74e8636445ba05d13283e43b5c716802278653
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 14:58:28 -0300

    fixing task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
-    if my_list is None:
-        return
-
-    if ((idx < 0) or (idx > len(my_list))):
-        return my_list
-
-    new_variable_list = my_list.copy()
-
-    new_variable_list[idx] = element
-    return new_variable_list
+    if my_list:
+        a = my_list.copy()
+        if idx < 0 or idx >= len(my_list):
+            return a
+        else:
+            a[idx] = element
+            return a
