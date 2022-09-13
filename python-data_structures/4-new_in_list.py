febe325a6402acf318e8e86e5c2944fc542c404f
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-13 11:07:19 -0300

    fixing whitesapces

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,8 +4,11 @@
 def new_in_list(my_list, idx, element):
     if my_list is None:
         return
+
     if ((idx < 0) or (idx > len(my_list))):
         return my_list
-    newlist = my_list.copy()
-    newlist[idx] = element
-    return newlist
+
+    new_variable_list = my_list.copy()
+
+    new_variable_list[idx] = element
+    return new_variable_list
