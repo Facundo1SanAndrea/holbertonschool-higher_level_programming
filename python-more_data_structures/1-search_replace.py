654e798c4e16e4f8bd0509ba6717c5a071fda6dd
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-15 14:04:49 -0300

    Fixing range of function, too long

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,6 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return
 
-    new_list = [replace if element == search else element for element in my_list]
+    new_list = [replace if element == search 
+else element for element in my_list]
     return new_list
