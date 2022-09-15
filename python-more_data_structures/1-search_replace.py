4f357b5082a9669eaa4ede1c2f04e0f8fa248954
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-15 14:06:14 -0300

    Handeling Errors

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,6 +5,5 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return
 
-    new_list = [replace if element == search 
-else element for element in my_list]
+    new_list = [replace if switch == search else switch for switch in my_list]
     return new_list
