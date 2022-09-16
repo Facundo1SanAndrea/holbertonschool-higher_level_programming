51c2cc86bfd3ce171d29b86fc93c706074c9d083
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-16 14:26:31 -0300

    Adding function that returns a key with the biggest integer value.

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+from re import I
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    Newvalue = 0
+    NewSpaces = ""
+    for i in a_dictionary.keys():
+        if a_dictionary[i] > Newvalue:
+            Newvalue = a_dictionary[i]
+            NewSpaces = I
+
+    return NewSpaces
