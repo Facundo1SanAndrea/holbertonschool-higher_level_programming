7388e0472a7f50a186aa158d80c2eeab6ef3ae0e
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-16 14:09:10 -0300

    Adding function that deletes a key in a dictionary.

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is None:
+        return
+    else:
+        pop = a_dictionary
+        pop.pop(key)
+        return pop
