f88af8d41fd8a9dbe1e1f7fdb0bb92b65d029f0a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-16 13:58:46 -0300

    Adding function that replaces or adds key/value in a dictionary.

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+from tkinter import N
+
+
+def update_dictionary(a_dictionary, key, value):
+    if a_dictionary is None or key is   None or value is None:
+        return
+
+    updated_dictonary = a_dictionary
+    updated_dictonary.update({key: value})
+    return updated_dictonary
