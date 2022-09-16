d94347bddd5f577590805fe1cab27b6c6b24a59a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-16 14:01:43 -0300

    Fixing white spaces

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None or key is   None or value is None:
+    if a_dictionary is None or key is None or value is None:
         return
 
     updated_dictonary = a_dictionary
