f3841f63408c4c740a7a5c6ebaf9c966f98ba964
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-16 14:09:56 -0300

    Fixing Return

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -3,7 +3,7 @@
 
 def simple_delete(a_dictionary, key=""):
     if a_dictionary.get(key) is None:
-        return
+        return a_dictionary
     else:
         pop = a_dictionary
         pop.pop(key)
