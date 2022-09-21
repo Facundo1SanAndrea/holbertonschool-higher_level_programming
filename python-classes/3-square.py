1ec2b4e52991b8b82d83faaac787390a4f67c7cb
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-21 15:43:01 -0300

    Adding a class Square that defines a square by: (based on 2-square.py)

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """class that defines a square for private attribute instance size"""
+    def __init__(self, size=0):
+        """Init class"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        """Area of the square"""
+        return (self.__size * self.__size)
