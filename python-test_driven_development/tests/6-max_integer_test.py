1486e14eab9a2468248e8e7e7c00764930285e6b
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-22 17:58:18 -0300

    redoing task 5 probelms of msg

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,28 +1,44 @@
 #!/usr/bin/python3
-"""test of unity for import"""
+"""
+Unittest for max_integer
+"""
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMaxInteger(unittest.TestCase):
-    """creat test for testing the function and it correct output"""
+class Test_Max_Integer(unittest.TestCase):
+    "Unittests for max_integer"
 
-    def test_1(self):
-        with self.assertRaises(TypeError):
-            max_integer(['tati', 7, 9, 2])
 
-    def test_2(self):
+    def test_empty_list(self):
+        """Test empty list"""
         self.assertEqual(max_integer([]), None)
 
-    def test_3(self):
-        self.assertEqual(max_integer([3, 5, 8, 43, 77, 89]), 89)
+    def test_classic_list(self):
+        """Test classic list"""
+        self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
 
-    def test_4(self):
-        self.assertEqual(max_integer([19, 19, 19, 19, 19]), 19)
+    def test_unor_list(self):
+        """Test unor list"""
+        self.assertEqual(max_integer([7, 6, 1, -2, 6]), 7)
 
-    def test_5(self):
-        self.assertEqual(max_integer([-7, -9, 2, 37, -44, 52]), 52)
+    def test_negative(self):
+        """Test negative"""
+        self.assertEqual(max_integer([-10, -6, -1, -100, -5]), -1)
 
+    def test_float(self):
+        """Test float"""
+        self.assertEqual(max_integer([9.9]), 9.9)
+
+    def test_none(self):
+        """Test none"""
+        self.assertRaises(TypeError, max_integer, None)
+
+    def test_strings(self):
+        """Test strings"""
+        self.assertEqual(max_integer(["low", "iq"]), "low")
 
 if __name__ == '__main__':
     unittest.main()
