c87428ca1345a2b7961dd1253bb954fbda0c5f3c
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-23 15:24:57 -0300

    rewatching code , tester wrong 4 test

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""class Rectangle that defines a rectangle """
-
+"""class that defines a rectangle in it """
 
 
 class Rectangle:
-    """class of a rectangle defines a rectangle"""
+    """Class of the rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """retirve width"""
-        return (self._width)
+        """retirbe the width"""
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -20,12 +19,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        """retrive height"""
-        return (self._height)
+        """retrives the height"""
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -33,4 +32,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
