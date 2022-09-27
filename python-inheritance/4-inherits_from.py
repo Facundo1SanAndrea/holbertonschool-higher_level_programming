4a518d7a730f03eb696f271e0e3a05e4bc565dd6
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-09-27 14:49:22 -0300

    Fixing long type

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" function that returns True if the object is an instance of a class that inherited"""
+""" function returns True if the object an instance of a class that inherited"""
 
 
 def inherits_from(obj, a_class):
