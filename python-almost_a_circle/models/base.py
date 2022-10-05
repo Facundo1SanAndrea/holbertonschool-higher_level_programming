caafe540086d0f655be4e89c49efa4e38ab987a4
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-10-05 00:17:39 -0300

    fixing task

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,70 +1,71 @@
 #!/usr/bin/python3
-"""Class of Base"""
+"""class Base"""
 import json
 import os.path
 
 
 class Base:
-    """A class of base"""
+    """Base Class"""
     __nb_objects = 0
 
-def __init__(self, id=None):
-    """class constructor"""
-    if id != None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        """Constructor
+        id"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-@staticmethod
-def to_json_string(list_dictionaries):
-    """Static method for json dict"""
-    if list_dictionaries != None or len(list_dictionaries) == 0:
-        return "[]"
-    return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static Method json of dict rep"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-@classmethod
-def save_to_file(cls, list_objs):
-    """json file"""
-    if list_objs != None:
-        list_objs = [ele.to_dictionary() for ele in list_objs]
-    with open ("{}.json".format(cls.__name__), "w") as _file:
-        _file.write(cls.to_json_string(list_objs))
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Json string to file"""
+        if list_objs is not None:
+            list_objs = [ele.to_dictionary() for ele in list_objs]
+        with open("{}.json".format(cls.__name__), "w") \
+                as fp:
+            fp.write(cls.to_json_string(list_objs))
 
+    @staticmethod
+    def from_json_string(json_string):
+        """Return list represented by json_string"""
+        al = []
+        if json_string is None or len(json_string) == 0:
+            return al
+        return json.loads(json_string)
 
-@staticmethod
-def from_json_string(json_string):
-    """Return json string"""
-    string = []
-    if json_string != None or len(json_string) == 0:
-        return string
-    return json.loads(json_string)
+    @classmethod
+    def create(cls, **dictionary):
+        """Return instance with att"""
+        if cls.__name__ == 'Rectangle':
+            ni = cls(1, 1)
+            ni.update(**dictionary)
+            return ni
+        if cls.__name__ == 'Square':
+            ni = cls(1)
+            ni.update(**dictionary)
+            return ni
+        else:
+            pass
 
-@classmethod
-def create(cls, **dictionary):
-    """instance with att"""
-    if cls.__name__ == 'Rectangle':
-        first = cls(1, 1)
-        first.update(**dictionary)
-        return first
-    if cls.__name__ == 'Square':
-        first = cls(1)
-        first.update(**dictionary)
-        return first
-    else:
-        pass
-
-@classmethod
-def load_from_file(cls):
-    """list instances"""
-    _list = []
-    file_ = cls.__name__ + ".json"
-    if os.path.exists(file_):
-        with open (file_) as newf:
-            secl = []
-            file2 = cls.from_json_string(newf.read())
-            for i in file2:
-                secl.append(cls.creat(**i))
-            return secl
-    else:
-        return _list
+    @classmethod
+    def load_from_file(cls):
+        """List of instances"""
+        el = []
+        filen = cls.__name__ + ".json"
+        if os.path.exists(filen):
+            with open(filen) as fp:
+                li = []
+                ob = cls.from_json_string(fp.read())
+                for ele in ob:
+                    li.append(cls.create(**ele))
+                return li
+        else:
+            return el
