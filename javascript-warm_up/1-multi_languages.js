6e84e95564152a665516243b7627ca58afb27c9a
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-06 15:13:18 -0300

    ADDING 1 AGAIN

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+/* task 1 */
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
 
-const said = 'C is fun';
-            'Python is cool';
-            'JavaScript is amazing';
-console.log(said);
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
