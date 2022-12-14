a3e0be9510d601c2b45e7605c3b6630c3c97e6dd
Facundo1SanAndrea <sanandrea.f@gmail.com>
2022-12-14 15:06:03 -0300

    adding task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Reads and prints the content of a file
+
+const path = process.argv[2]
+const fs = require('fs')
+
+fs.readFile(path, utf-8, (err, data) => {
+    if (err) {
+        console.log(err);
+    }  else {
+        console.log(data)
+    }
+});
